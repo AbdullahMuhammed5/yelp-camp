82b8986fc0aaffdefcff7da39282b3a10bf9dfd5
Abdullah Muhammed <abdo56668@gmail.com>
2019-07-23 17:10:04 +0200

    adding views, partials and some initial styles

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
 var express = require('express')
-	app = express();
+	app = express()
+	bodyParser = require('body-parser');
 
 var port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res)=>{
 	res.render('landing')
 })
 
+const campgrounds = [
+	{ name: 'Salamon Greek', image: 'https://pixabay.com/get/54e6d0434957a514f6da8c7dda793f7f1636dfe2564c704c732c7cdd9244c05f_340.jpg'},
+	{ name: 'Sante Catrine', image: 'https://pixabay.com/get/57e8d0424a5bae14f6da8c7dda793f7f1636dfe2564c704c732c7cdd9244c05f_340.jpg'},
+	{ name: 'El Rayan Vally', image: 'https://pixabay.com/get/57e2d54b4852ad14f6da8c7dda793f7f1636dfe2564c704c732c7cdd9244c05f_340.jpg'},
+	{ name: 'Mountian Goat\'s Rest' , image: 'https://pixabay.com/get/57e8d1454b56ae14f6da8c7dda793f7f1636dfe2564c704c732c7cdd9244c05f_340.jpg'},
+]
+
 app.get('/campgrounds', (req, res)=>{
-	const campgrounds = [
-		{ name: 'Salamon Greek', image: 'https://pixabay.com/get/54e6d0434957a514f6da8c7dda793f7f1636dfe2564c704c732c7cdd9244c05f_340.jpg'},
-		{ name: 'Sante Catrine', image: 'https://pixabay.com/get/57e8d0424a5bae14f6da8c7dda793f7f1636dfe2564c704c732c7cdd9244c05f_340.jpg'},
-		{ name: 'El Rayan Vally', image: 'https://pixabay.com/get/57e2d54b4852ad14f6da8c7dda793f7f1636dfe2564c704c732c7cdd9244c05f_340.jpg'},
-		{ name: 'Mountian Goat\'s Rest' , image: 'https://pixabay.com/get/57e8d1454b56ae14f6da8c7dda793f7f1636dfe2564c704c732c7cdd9244c05f_340.jpg'},
-	]
 	res.render('campgrounds', {campgrounds: campgrounds})
+})
+
+app.get('/campgrounds/new', (req, res)=>{
+	res.render('new')
+})
+
+app.post('/campgrounds', (req, res)=>{
+	// get data form form request and add it to campgrounds array
+	var newCampground = { name: req.body.name, image: req.body.image }
+	campgrounds.push(newCampground)
+	// redirect to campgrounds page
+	res.redirect('/campgrounds')
 })
 
 app.listen(port, ()=>{
